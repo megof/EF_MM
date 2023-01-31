b61bfc22c9432378455b74547b0416690a1edbad
David Ospina <J_do_o13@hotmail.com>
2023-01-30 22:37:47 -0500

    add varials
    se añadieron variables necesarias para las secciones

--- js/back.js
+++ js/back.js
@@ -3,8 +3,13 @@ var app = new Vue({
     data:{
         message:"",
         ls_apt:"",
+        ls_adm:"",
         pin_apt:"",
-        tre: 1<0,
+        salary:[1800000,1500000,1300000],
+        cnt_shoes:0,
+        prc_shoes:5000,
+        gain:1000,
+        
     },
     methods:{
         guardarDatos(){
