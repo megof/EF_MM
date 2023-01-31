4a490a16de239125d0882227d4a1a858a1201d54
David Ospina <J_do_o13@hotmail.com>
2023-01-31 00:36:43 -0500

    Update functions - register employes
    se añadió la función correspondiente al registro de cada uno de los empleados

--- js/back.js
+++ js/back.js
@@ -13,11 +13,36 @@ var app = new Vue({
         sbd_tr:0,//subsidio de transporte
         vnt:0,//ventas de zapatos (revisar)
         nm_zpt:0,//número de zapatos ensamblados
+        nm_zpl:0,//número de zapatillas ensamblados
         hj:0,//hijos ensamblador
+        employes:[],
     },
     methods:{
         guardarDatos(){
-
-        }
-    }
+            switch (this.ls_apt) {
+                case "Ensamblador":
+                    this.employes.push({
+                        type: this.ls_apt,
+                        ex: this.hr_ex,
+                        zpt: this.nm_zpt,
+                        zpl: this.nm_zpl,
+                        hj: this.hj,
+                    })
+                    break;
+                case "Secretario":
+                    this.employes.push({
+                        type: this.ls_apt,
+                        ex: this.hr_ex,
+                    })
+                    break;
+                case "Vendedor":
+                    this.employes.push({
+                        type: this.ls_apt,
+                    })
+                    break;            
+                default:
+                    break;
+            }
+        },
+    },
 })
