4bd8829ab411853ad09b12161038d8717b2dcb07
David Ospina <J_do_o13@hotmail.com>
2023-01-30 22:02:14 -0500

    add v-if, repair error v-model
    se añadieron v-if para que dependiendo del rol que se ingrese se vea una sección en específico, se repararon errores de id app - se necesita corregir variables vue para los nuevos componentes

--- js/back.js
+++ js/back.js
@@ -1,10 +1,14 @@
 var app = new Vue({
     el:'#app',
     data:{
+        message:"",
         ls_apt:"",
         pin_apt:"",
+        tre: 1<0,
     },
     methods:{
+        guardarDatos(){
 
+        }
     }
 })
