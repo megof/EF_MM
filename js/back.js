d551c6f190850a51576231d11e50b32f69f287a1
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-01-31 00:13:58 -0500

    update

--- js/back.js
+++ js/back.js
@@ -2,13 +2,18 @@ var app = new Vue({
     el:'#app',
     data:{
         message:"",
-        ls_apt:"Secretario",
-        ls_adm:0,
-        pin_apt:"",
-        salary:[1800000,1500000,1300000],
-        cnt_shoes:0,
-        prc_shoes:5000,
-        gain:1000,       
+        ls_apt:"Administrador",//tipo de usuario
+        ls_adm:0,//tipo de usuario para modificaciones del administrador
+        pin_apt:"",//pin de usuarios
+        salary:[1800000,1500000,1300000],//salarios de los empleados (Ensamblador, Secretario, Vendedor)
+        cnt_shoes:0,//cantidad máxima de zapatos permitida
+        prc_shoes:5000,//costo de ensamble
+        gain:1000,//comisión por venta
+        hr_ex:0,//hora extra secretario/ensamblador
+        sbd_tr:0,//subsidio de transporte
+        vnt:0,//ventas de zapatos (revisar)
+        nm_zpt:0,//número de zapatos ensamblados
+        hj:0,//hijos ensamblador
     },
     methods:{
         guardarDatos(){
